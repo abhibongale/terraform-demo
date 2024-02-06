a689a37ee253cbc9c6d9b7b44b7484dd9e135ffd
abhibongale <abhishekbongale@outlook.com>
2024-02-06 09:58:42 +0000

    single-web-server

--- single-web-server/main.tf
+++ single-web-server/main.tf
@@ -2,9 +2,26 @@ provider "aws" {
     region = "us-east-1"
 }
 
+resource "aws_security_group" "demo-group-instance" {
+    name = "terraform-demo-server"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 resource "aws_instance" "demo-server" {
     ami = "ami-0c24ee2a1e3b9df45"
     instance_type = "t2.micro"
+    security_groups = ["${aws_security_group.demo-group-instance.id}"]
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "hello-world" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
 
     tags = {
         Name = "terraform-server"
